ba49dcc62f2f8c29a7f199312d48a76d10e190ab
Dev Aggarwal <devxpy@gmail.com>
2019-05-01 15:10:24 +0530

    tokens stuff

--- android/src/main/kotlin/com/pycampers/flutter_cognito_plugin/Cognito.kt
+++ android/src/main/kotlin/com/pycampers/flutter_cognito_plugin/Cognito.kt
@@ -4,13 +4,12 @@ import android.content.Context
 import com.amazonaws.mobile.client.AWSMobileClient
 import com.amazonaws.mobile.client.Callback
 import com.amazonaws.mobile.client.UserStateDetails
-import com.amazonaws.mobile.client.results.ForgotPasswordResult
-import com.amazonaws.mobile.client.results.SignInResult
-import com.amazonaws.mobile.client.results.SignUpResult
-import com.amazonaws.mobile.client.results.UserCodeDeliveryDetails
+import com.amazonaws.mobile.client.results.*
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.Result
+
+const val TAG = "FlutterCognitoPlugin"
 
 class Cognito(val context: Context, val methodChannel: MethodChannel) : MethodCallDispatcher() {
     val awsClient = AWSMobileClient.getInstance()!!
@@ -21,12 +20,11 @@ class Cognito(val context: Context, val methodChannel: MethodChannel) : MethodCa
                 awsClient.addUserStateListener {
                     methodChannel.invokeMethod("userStateCallback", dumpUserState(it))
                 }
-                initialized = true
-                result.success(dumpUserState(u))
+                trySend(result) { dumpUserState(u) }
             }
 
             override fun onError(e: Exception) {
-                throw e
+                sendError(e, result)
             }
         })
     }
@@ -37,8 +35,8 @@ class Cognito(val context: Context, val methodChannel: MethodChannel) : MethodCa
         val userAttributes = call.argument<Map<String, String>>("userAttributes")
 
         awsClient.signUp(username, password, userAttributes, null, object : Callback<SignUpResult> {
-            override fun onResult(s: SignUpResult) = result.success(dumpSignUpResult(s))
-            override fun onError(e: Exception) = dumpException(e, result)
+            override fun onResult(s: SignUpResult) = trySend(result) { dumpSignUpResult(s) }
+            override fun onError(e: Exception) = sendError(e, result)
         })
     }
 
@@ -47,8 +45,8 @@ class Cognito(val context: Context, val methodChannel: MethodChannel) : MethodCa
         val confirmationCode = call.argument<String>("confirmationCode")
 
         awsClient.confirmSignUp(username, confirmationCode, object : Callback<SignUpResult> {
-            override fun onResult(s: SignUpResult) = result.success(dumpSignUpResult(s))
-            override fun onError(e: Exception) = dumpException(e, result)
+            override fun onResult(s: SignUpResult) = trySend(result) { dumpSignUpResult(s) }
+            override fun onError(e: Exception) = sendError(e, result)
         })
     }
 
@@ -56,8 +54,8 @@ class Cognito(val context: Context, val methodChannel: MethodChannel) : MethodCa
         val username = call.argument<String>("username")
 
         awsClient.resendSignUp(username, object : Callback<SignUpResult> {
-            override fun onResult(s: SignUpResult) = result.success(dumpSignUpResult(s))
-            override fun onError(e: Exception) = dumpException(e, result)
+            override fun onResult(s: SignUpResult) = trySend(result) { dumpSignUpResult(s) }
+            override fun onError(e: Exception) = sendError(e, result)
         })
     }
 
@@ -66,8 +64,8 @@ class Cognito(val context: Context, val methodChannel: MethodChannel) : MethodCa
         val password = call.argument<String>("password")
 
         awsClient.signIn(username, password, null, object : Callback<SignInResult> {
-            override fun onResult(s: SignInResult) = result.success(dumpSignInResult(s))
-            override fun onError(e: Exception) = dumpException(e, result)
+            override fun onResult(s: SignInResult) = trySend(result) { dumpSignInResult(s) }
+            override fun onError(e: Exception) = sendError(e, result)
         })
     }
 
@@ -75,8 +73,8 @@ class Cognito(val context: Context, val methodChannel: MethodChannel) : MethodCa
         val confirmationCode = call.argument<String>("confirmationCode")
 
         awsClient.confirmSignIn(confirmationCode, object : Callback<SignInResult> {
-            override fun onResult(s: SignInResult) = result.success(dumpSignInResult(s))
-            override fun onError(e: Exception) = dumpException(e, result)
+            override fun onResult(s: SignInResult) = trySend(result) { dumpSignInResult(s) }
+            override fun onError(e: Exception) = sendError(e, result)
         })
     }
 
@@ -84,8 +82,11 @@ class Cognito(val context: Context, val methodChannel: MethodChannel) : MethodCa
         val username = call.argument<String>("username")
 
         awsClient.forgotPassword(username, object : Callback<ForgotPasswordResult> {
-            override fun onResult(f: ForgotPasswordResult) = result.success(dumpForgotPasswordResult(f))
-            override fun onError(e: Exception) = dumpException(e, result)
+            override fun onResult(f: ForgotPasswordResult) {
+                trySend(result) { dumpForgotPasswordResult(f) }
+            }
+
+            override fun onError(e: Exception) = sendError(e, result)
         })
     }
 
@@ -94,15 +95,18 @@ class Cognito(val context: Context, val methodChannel: MethodChannel) : MethodCa
         val confirmationCode = call.argument<String>("confirmationCode")
 
         awsClient.confirmForgotPassword(newPassword, confirmationCode, object : Callback<ForgotPasswordResult> {
-            override fun onResult(f: ForgotPasswordResult) = result.success(dumpForgotPasswordResult(f))
-            override fun onError(e: Exception) = dumpException(e, result)
+            override fun onResult(f: ForgotPasswordResult) {
+                trySend(result) { dumpForgotPasswordResult(f) }
+            }
+
+            override fun onError(e: Exception) = sendError(e, result)
         })
     }
 
     fun getUserAttributes(call: MethodCall, result: Result) {
         awsClient.getUserAttributes(object : Callback<Map<String, String>> {
-            override fun onResult(attrs: Map<String, String>?) = result.success(attrs)
-            override fun onError(e: Exception) = dumpException(e, result)
+            override fun onResult(attrs: Map<String, String>?) = trySend(result) { attrs }
+            override fun onError(e: Exception) = sendError(e, result)
         })
     }
 
@@ -110,8 +114,11 @@ class Cognito(val context: Context, val methodChannel: MethodChannel) : MethodCa
         val userAttributes = call.argument<Map<String, String>>("userAttributes")!!
 
         awsClient.updateUserAttributes(userAttributes, object : Callback<List<UserCodeDeliveryDetails>> {
-            override fun onResult(u: List<UserCodeDeliveryDetails>) = result.success(u.map { dumpUserCodeDeliveryDetails(it) })
-            override fun onError(e: Exception) = dumpException(e, result)
+            override fun onResult(u: List<UserCodeDeliveryDetails>) {
+                trySend(result) { u.map { dumpUserCodeDeliveryDetails(it) } }
+            }
+
+            override fun onError(e: Exception) = sendError(e, result)
         })
     }
 
@@ -120,57 +127,44 @@ class Cognito(val context: Context, val methodChannel: MethodChannel) : MethodCa
         val confirmationCode = call.argument<String>("confirmationCode")!!
 
         awsClient.confirmUpdateUserAttribute(attributeName, confirmationCode, object : Callback<Void> {
-            override fun onResult(u: Void) = result.success(u)
-            override fun onError(e: Exception) = dumpException(e, result)
+            override fun onResult(u: Void) = trySend(result) { (u) }
+            override fun onError(e: Exception) = sendError(e, result)
         })
     }
 
     fun signOut(call: MethodCall, result: Result) {
-        try {
-            awsClient.signOut()
-        } catch (e: Throwable) {
-            return dumpException(e, result)
-        }
-        result.success(null)
+        trySend(result) { awsClient.signOut() }
     }
 
     fun getUsername(call: MethodCall, result: Result) {
-        val value: String
-        try {
-            value = awsClient.username
-        } catch (e: Throwable) {
-            return dumpException(e, result)
-        }
-        result.success(value)
+        trySend(result) { awsClient.username }
     }
 
     fun isSignedIn(call: MethodCall, result: Result) {
-        val value: Boolean
-        try {
-            value = awsClient.isSignedIn
-        } catch (e: Throwable) {
-            return dumpException(e, result)
-        }
-        result.success(value)
+        trySend(result) { awsClient.isSignedIn }
     }
 
     fun getIdentityId(call: MethodCall, result: Result) {
-        val value: String
-        try {
-            value = awsClient.identityId
-        } catch (e: Throwable) {
-            return dumpException(e, result)
-        }
-        result.success(value)
+        trySend(result) { awsClient.identityId }
     }
 
     fun currentUserState(call: MethodCall, result: Result) {
-        val value: UserStateDetails
-        try {
-            value = awsClient.currentUserState()
-        } catch (e: Throwable) {
-            return dumpException(e, result)
-        }
-        result.success(dumpUserState(value))
+        trySend(result) { awsClient.currentUserState()?.userState?.ordinal }
+    }
+
+    fun getTokens(call: MethodCall, result: Result) {
+        awsClient.getTokens(object : Callback<Tokens> {
+            override fun onResult(t: Tokens?) {
+                trySend(result) {
+                    listOf(
+                            t?.accessToken?.tokenString,
+                            t?.idToken?.tokenString,
+                            t?.refreshToken?.tokenString
+                    )
+                }
+            }
+
+            override fun onError(e: Exception) = sendError(e, result)
+        })
     }
 }
--- android/src/main/kotlin/com/pycampers/flutter_cognito_plugin/MethodCallDispatcher.kt
+++ android/src/main/kotlin/com/pycampers/flutter_cognito_plugin/MethodCallDispatcher.kt
@@ -1,6 +1,8 @@
 package com.pycampers.flutter_cognito_plugin
 
 import android.os.AsyncTask
+import android.util.Log
+import com.amazonaws.AmazonServiceException
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
@@ -16,19 +18,63 @@ class DoAsync(val fn: () -> Unit) : AsyncTask<Void, Void, Void>() {
     }
 }
 
-open class MethodCallDispatcher : MethodCallHandler {
-    var initialized = false
+/*
+run `fn`, ignoring `IllegalStateException`.
+(workaround for https://github.com/flutter/flutter/issues/29092.)
+*/
+fun ignoreIllegalState(fn: () -> Unit) {
+    try {
+        fn()
+    } catch (e: IllegalStateException) {
+        Log.d(TAG, "ignoring exception: $e. See https://github.com/flutter/flutter/issues/29092 for details.")
+    }
+}
 
+/*
+try to send the value returned by `fn()` using `result`.
+sends an error using `sendError` if required.
+*/
+fun <T> trySend(result: Result, fn: () -> T) {
+    val value: T
+    try {
+        value = fn()
+    } catch (e: Throwable) {
+        sendError(e, result)
+        return
+    }
+
+    ignoreIllegalState {
+        if (value is Unit) {
+            result.success(null)
+        } else {
+            result.success(value)
+        }
+    }
+}
+
+/* pipe the exception `e` back to flutter using `result.error()`. */
+fun sendError(e: Throwable, result: Result) {
+    Log.d(TAG, "piping exception to flutter: $e")
+    e.printStackTrace()
+    try {
+        throw e
+    } catch (e: AmazonServiceException) {
+        ignoreIllegalState { result.error(e.errorCode, e.errorMessage, e.errorType.name) }
+    } catch (e: Throwable) {
+        ignoreIllegalState { result.error(e.javaClass.canonicalName, e.message, null) }
+    }
+}
+
+/*
+inherit this class to make any kotlin methods with the signature:-
+
+    `methodName(io.flutter.plugin.common.MethodCall, io.flutter.plugin.common.MethodChannel.Result)`
+
+be magically available to flutter, by the power of dynamic dispatch!
+*/
+open class MethodCallDispatcher : MethodCallHandler {
     override fun onMethodCall(call: MethodCall, result: Result) {
         val methodName = call.method
-
-        if (!initialized && methodName != "initialize") {
-            return result.error(
-                    "NotYetInitialized",
-                    "You must call `initialize()` before using this plugin!",
-                    null
-            )
-        }
 
         val method = try {
             javaClass.getMethod(methodName, MethodCall::class.java, Result::class.java)
@@ -41,8 +87,16 @@ open class MethodCallDispatcher : MethodCallHandler {
 
         if (method == null) {
             result.notImplemented()
-        } else {
-            DoAsync { method.invoke(this, call, result) }
+            return
+        }
+
+        DoAsync {
+            Log.d(TAG, "invoking { ${javaClass.simpleName}.$methodName() }...")
+            try {
+                ignoreIllegalState { method.invoke(this, call, result) }
+            } catch (e: Throwable) {
+                sendError(e, result)
+            }
         }
     }
 }
