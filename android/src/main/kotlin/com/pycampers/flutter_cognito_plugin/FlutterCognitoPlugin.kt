db9e3168d88e3b0d16aa9d6dbb129e6b779d7edb
Dev Aggarwal <devxpy@gmail.com>
2019-08-06 19:25:52 +0530

    update plugin_scaffold

--- android/src/main/kotlin/com/pycampers/flutter_cognito_plugin/FlutterCognitoPlugin.kt
+++ android/src/main/kotlin/com/pycampers/flutter_cognito_plugin/FlutterCognitoPlugin.kt
@@ -1,6 +1,7 @@
 package com.pycampers.flutter_cognito_plugin
 
 import com.pycampers.plugin_scaffold.createPluginScaffold
+import com.pycampers.plugin_scaffold.handler
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
 const val PKG_NAME = "com.pycampers.flutter_cognito_plugin"
@@ -12,7 +13,9 @@ class FlutterCognitoPlugin {
             val plugin = Cognito(registrar.context())
             val channel = createPluginScaffold(registrar.messenger(), PKG_NAME, plugin)
             plugin.awsClient.addUserStateListener {
-                channel.invokeMethod("userStateCallback", dumpUserState(it))
+                handler.post {
+                    channel.invokeMethod("userStateCallback", dumpUserState(it))
+                }
             }
         }
     }
